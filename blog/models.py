e14279fc4547f984961d3962f2024a79f80b101b
John Marcos <john10112010@hotmail.com>
2019-03-26 03:09:19 -0300

    My Django Girls app, first commit

---
+++ blog/models.py
@@ -0,0 +1,20 @@
+# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model):
+    autor = models.ForeignKey('auth.User', on_delete=models.CASCADE) #link para outro modelo
+    titulo = models.CharField(max_length=200) #caracteres limitados
+    texto = models.TextField() #string sem limites
+    data_criacao = models.DateTimeField( #formato para datas
+            default=timezone.now)
+    data_publicacao = models.DateTimeField(
+            blank=True, null=True)
+
+    def publicar(self):
+        self.data_publicacao = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.titulo
---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\pc1\documents\sistema_web_pi2\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
